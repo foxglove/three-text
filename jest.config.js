e222b196c0c635862d479134e91302702f614cc9
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-01-10 16:37:24 -0800

    bring back testMatch

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ const { createDefaultEsmPreset } = require("ts-jest");
 /** @type {import("ts-jest").JestConfigWithTsJest} */
 module.exports = {
   ...createDefaultEsmPreset({}),
+  testMatch: ["<rootDir>/src/**/*.test.ts"],
   setupFiles: ["jest-canvas-mock"],
 };
