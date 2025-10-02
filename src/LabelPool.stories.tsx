7afb3e91cd23625f9dc1715b3e0893eb622ef527
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-10-02 11:57:45 -0700

    update

--- src/LabelPool.stories.tsx
+++ src/LabelPool.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react-webpack5";
 import { useEffect, useRef, useState, type ReactElement } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
